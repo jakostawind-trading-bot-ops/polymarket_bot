da29b4465708cdedb383b861f9407b240f36057a
JaKoSTaWiND <kachkoroman2005@gmail.com>
2026-09-07 23:51:54 +0500

    main.py отрефакторен: убрана run(), все инициализации внутри main()

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,13 +1,15 @@
 import asyncio
 
-from bot.bootstrap import BootstrapSettings, parse_cli_args
+from bot.bootstrap import parse_cli_args
 from bot.nats.publisher import NatsPublisher
 from bot.di.container import create_container
 from bot.lifecycle.process_lifecycle.process_lifecycle import ProcessLifecycle
 
 
-async def run(bootstrap_settings: BootstrapSettings) -> None:
-    async with create_container(bootstrap_settings) as container:
+async def main() -> None:
+    settings = parse_cli_args()
+
+    async with create_container(settings) as container:
         publisher = await container.get(NatsPublisher)
         
         process_lifecycle = ProcessLifecycle()
@@ -20,10 +22,6 @@ async def run(bootstrap_settings: BootstrapSettings) -> None:
         await process_lifecycle.wait_for_shutdown()
 
 
-def main() -> None:
-    settings = parse_cli_args()
-    asyncio.run(run(settings))
-
-
 if __name__ == "__main__":
+    asyncio.run(main())
     main()
